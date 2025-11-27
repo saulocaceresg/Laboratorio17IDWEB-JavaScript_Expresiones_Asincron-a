71058d468ab5e13aba42a9f41d6065bb7062f222
saulocaceresg <scaceresg@unsa.edu.pe>
2025-11-27 15:29:35 -0500

    Commit 3: segundo ejercicio (4.) resuelto -> SyntaxError

--- public-html/scripts/script.js
+++ public-html/scripts/script.js
@@ -7,3 +7,17 @@ try {
 } catch (e) {
     console.error(e.message)
 }
+
+// 4.	Generar un SyntaxError usando JSON.parse con texto inválido. En el catch mostrar e.name y e.message
+
+console.log("Ejercicio 2 (4.)\nJSON inválido");
+
+try {
+    let textoJSON = "{nombre: 'Pedro'}";
+    let objeto = JSON.parse(textoJSON);
+    console.log("Objeto: ", objeto);
+} catch (e) {
+    console.log(e.name);
+    console.log(e.message);
+}
+
