0a892fbdb6d5f2864f6fbeb0800299b00ece3920
saulocaceresg <scaceresg@unsa.edu.pe>
2025-11-27 20:06:38 -0500

    Commit 7: sexto ejercicio (8.) resuelto -> Captura de error por niveles

--- public-html/scripts/script.js
+++ public-html/scripts/script.js
@@ -74,3 +74,33 @@ try {
     }
 }
 
+// 8.	En un catch dentro de una función imprimir un mensaje de error y luego volver a lanzar el mismo error con throw hacia otra función que la invocó. El error también será capturado en esa otra función y finalmente será capturado en un catch externo a cualquier de las funciones, el cual lo recibe e imprime. Comprueba que el error sigue propagándose
+// Salida: 
+// Nivel 2 atrapó el error: x is not defined 
+// Nivel 1 recibió el error: x is not defined 
+// ERROR FINAL capturado en el nivel superior: x is not defined
+console.log("+----------------------------+\nEjercicio 6 (8.)\n\"Captura sucesiva de error\"");
+
+function nivel1() {
+    try {
+        console.log(x);
+    } catch (e) {
+        console.error("Nivel 1 atrapó el error: ", e.message);
+        throw e;
+    }
+}
+
+function nivel2() {
+    try {
+        nivel1();
+    } catch (e) {
+        console.error("Nivel 2 atrapó el error: ", e.message);
+        throw e;
+    }
+}
+
+try {
+    nivel2();
+} catch (e) {
+    console.error("ERROR FINAL. Capturado en el nivel superior: ", e.message);
+}
